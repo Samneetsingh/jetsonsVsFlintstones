4b120de46053fac0fe8806df22dfe28df986a2f4
Samneet Singh <samneetdhillon@hotmail.com>
2016-04-28 15:36:32 -0400

    Added Moving Camera Script for Level 1.

---
+++ Assets/scripts/MoveCameraScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCameraScript : MonoBehaviour {
+
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () 
+	{
+		offset = new Vector3 (0.01f, 0f, 0f);
+	
+	}
+
+	void MoveCameraPosition()
+	{
+		transform.position = transform.position + offset;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		MoveCameraPosition ();
+	
+	}
+}
