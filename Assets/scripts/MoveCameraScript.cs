ceba2d4a65780e58589df8743a95136aef8ed150
Samneet Singh <samneetdhillon@hotmail.com>
2016-05-26 17:51:29 -0400

    Update

--- Assets/scripts/MoveCameraScript.cs
+++ Assets/scripts/MoveCameraScript.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class MoveCameraScript : MonoBehaviour {
 
-	private float cameraSpeed;
+	//private CharacterMovementController controller;
+	private Vector2 velocity;
+	private GameObject player;
 	// Use this for initialization
 	void Start () 
 	{
-	
+		player = GameObject.FindGameObjectWithTag ("Player");
+
+
 	}
 
-	void CheckCameraSpeed()
+	/*void CheckCameraSpeed()
 	{
 		if (Input.GetKey (KeyCode.R)) {
 			cameraSpeed = 2;
@@ -18,17 +22,17 @@ public class MoveCameraScript : MonoBehaviour {
 		{
 			cameraSpeed = 1;
 		}
-	}
+	}*/
 
 	void MoveCameraPosition()
 	{
 		if (Input.GetKey(KeyCode.RightArrow)) 
 		{
-			transform.position = transform.position + new Vector3 (0.02f * cameraSpeed, 0f, 0f);
+			transform.position = transform.position + new Vector3 (cameraVelocity, 0f, 0f);
 		}
 		else if (Input.GetKey(KeyCode.LeftArrow))
 		{
-			transform.position = transform.position + new Vector3 (-0.02f * cameraSpeed, 0f, 0f);
+			transform.position = transform.position + new Vector3 (-cameraVelocity, 0f, 0f);
 		}
 
 	}
@@ -36,7 +40,8 @@ public class MoveCameraScript : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
-		CheckCameraSpeed ();
+		Debug.Log (cameraVelocity);
+		//CheckCameraSpeed ();
 		MoveCameraPosition ();
 	
 	}
